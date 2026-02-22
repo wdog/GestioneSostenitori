99ab3633ae925eba7ba1436aa4349838fbd0b318
wdog <wdog666@gmail.com>
2026-02-22 20:46:59 +0100

    WIP

--- routes/telegram.php
+++ routes/telegram.php
@@ -4,20 +4,9 @@
 
 use App\Telegram\Commands\StartCommand;
 use App\Telegram\Menus\MainMenu;
-use Illuminate\Support\Facades\Log;
-use SergiX44\Nutgram\Nutgram;
+
 
 $bot->registerCommand(StartCommand::class);
 
 $bot->onCommand('menu', MainMenu::class)
     ->description('Apri il menu principale');
-
-
-$bot->fallback(function (Nutgram $bot) {
-    $text = $bot->message()?->text ?? '';
-    Log::info($text);
-    if (str_starts_with($text, '/')) {
-        Log::info("MATCH " . $text);
-        $bot->sendMessage('Non so come farlo. Prova a cercare nel /menu.');
-    }
-});
